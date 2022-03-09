9dcd85864f221622ec872d2ad17fd200b198146b
Joseph Supples <joseph.craig94@gmail.com>
2022-03-08 20:44:47 -0800

    Completed Exercises 1, 2, & 3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Name: Joseph Supples
 import React, { Component } from 'react';
 import './App.css';
 
@@ -70,11 +71,11 @@ class App extends Component {
 
         <h1>My Recipes</h1>
 
-        <Form /> {/*Modify it here EXERCISE 1 */}
+        <Form  onSave = {this.handleSave}/> {/*EXERCISE 1 */}
 
         <hr />
 
-        <List  />  {/*Modify it here EXERCISE 2 */}
+        <List  recipes = {this.state.recipes}/>  {/* EXERCISE 2 */}
       </div>
     );
   }
@@ -184,7 +185,12 @@ class Form extends Component {
   handleReset = (e) => {
     e.preventDefault();
     alert(`Are you sure you want to reset?`)
-   {/*Modify it here EXERCISE 3 */}
+    this.setState({
+      title: '',
+      instructions: "",
+      ingredients: [''],
+      img: ''
+    })
 }
 
   handleSubmit(e) {
